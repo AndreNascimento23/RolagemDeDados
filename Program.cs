f8061d8769d7b5f7daf343fa525b046934d836c7
André Nascimento <andre4.20nascimento@outlook.com>
2023-04-19 12:02:37 -0300

    Exercicio Terminado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+Console.Clear();
+
+Console.WriteLine(" --- Rolagem de Dado ---\n");
+
+int faces, resultado;
+
+Console.Write("Quantas faces do dado..:  ");
+faces = Int32.Parse(Console.ReadLine()!);
+
+Thread.Sleep(1000);
+
+resultado = RandomNumberGenerator.GetInt32(1 , faces+1);
+
+Console.Write($"\nNumero Sorteado: {resultado} ");
